5c816a5c99a3f2587fe6f1c4afa43ec07ac758d4
Andy <nievasandy73@gmail.com>
2020-08-24 18:17:47 -0300

    primer comit de eMercado por terminal

--- Workspace_inicial/js/products.js
+++ Workspace_inicial/js/products.js
@@ -6,7 +6,7 @@ var maxCount = undefined;
 function showProductsList(){
 
     let htmlContentToAppend = "";
-    let arrayTags_P5 = document.getElementsByClassName("p-5");
+    let arrayTags_P5 = document.getElementsByClassName("container");
     for(let i = 0; i < currentProductsArray.length; i++){
         let product = currentProductsArray[i];
 
@@ -17,22 +17,26 @@ function showProductsList(){
             <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
-                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                        <img src="` + product.imgSrc + `" alt="` + product.name + `" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
                             <h4 class="mb-1">`+ product.name +`</h4>
-                            <small class="text-muted">` + product.productCount + ` artículos</small>
+                            <small class="text-muted">` + product.soldCount + ` artículos</small>
                         </div>
-                        <p class="mb-1">` + product.description + `</p>
+                        <p class="mb-1">` + product.description +`</p>
+                        <p style="font-family: 'Audiowide'; font-weight: bold;"> ` + product.currency + ` - ` + product.cost + ` </p>
                     </div>
                 </div>
             </a>
             `
         }
 
-        arrayTags_P5[0].innerHTML = htmlContentToAppend;
     }
+    
+    arrayTags_P5[1].innerHTML += htmlContentToAppend;
+
+    arrayTags_P5[1].id = "p5";
 }
 
 
@@ -47,6 +51,10 @@ function ShowProducts(productsArray){
     
 }
 
+function ola(contenedor, contenedorParaBotones){
+    getElementById("buscador").value; // Agarrar el valor del input buscador para ordenar los elementos
+}
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -56,7 +64,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             ShowProducts(resultObj.data);
+            console.log(resultObj.data);
         }
     });
-
+    
 });
